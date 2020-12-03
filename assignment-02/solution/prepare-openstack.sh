001ac302b1c9f5bd213b0e36942f8e674efe54ab
Emirhan Karagül <emirhan.karaguel@dai-labor.de>
2020-12-03 01:07:42 +0100

    Trying out ssh

--- assignment-02/solution/prepare-openstack.sh
+++ assignment-02/solution/prepare-openstack.sh
@@ -12,7 +12,7 @@ openstack security group rule create --ingress --proto udp --remote-ip 0.0.0.0/0
 
 openstack security group rule create --ingress --proto icmp --remote-ip 0.0.0.0/0 open-all
 
-ssh-keygen -b 2048 -t rsa -f ./id_openstack -q -N ""
+ssh-keygen -b 2048 -t rsa -f ./id_openstack -q -N "" -C
 
 IP=$(gcloud compute instances list | grep controller | awk '{print $5}' | cut -d ',' -f 1)
 
@@ -20,9 +20,7 @@ scp -i ~/.ssh/id_rsa ./id_openstack $USER@$IP:/home/$USER/.ssh
 
 ssh -i ~/.ssh/id_rsa  $USER@$IP 'chmod 400 ~/.ssh/id_openstack'
 
-openstack keypair create --public-key ./id_openstack.pub openstack_default_key
-
-m1.medium  ubuntu-16.04 admin-net default
+openstack keypair create --public-key ./id_openstack.pub openstack_key
 
 PROJECT_ID=$(openstack project list | grep admin | awk '{print $2}')
 
@@ -30,4 +28,4 @@ SECURITY_ID=$(openstack security group list --project $PROJECT_ID | grep default
 
 openstack server create --flavor m1.medium --image ubuntu-16.04 \
     --network admin-net --security-group $SECURITY_ID \
-    --key-name openstack_default_key ins1
+    --key-name openstack_key ins1
